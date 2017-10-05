a598b168fb2b8298fa2396d2d69d33a7b2731cb9
FordMcLeod <omcleod@ualberta.ca>
2017-10-04 18:46:50 -0600

    Added Elasticsearch functionality.

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/ElasticsearchTweetController.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/ElasticsearchTweetController.java
@@ -7,7 +7,15 @@ import com.searchly.jestdroid.DroidClientConfig;
 import com.searchly.jestdroid.JestClientFactory;
 import com.searchly.jestdroid.JestDroidClient;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import io.searchbox.core.DocumentResult;
 import io.searchbox.core.Index;
+import io.searchbox.core.Search;
+import io.searchbox.core.SearchResult;
+import io.searchbox.core.search.aggregation.PercentileRanksAggregation;
+import
 
 /**
  * Created by romansky on 10/20/16.
@@ -26,8 +34,15 @@ public class ElasticsearchTweetController {
                 Index index = new Index.Builder(tweet).index("testing").type("tweet").build();
 
                 try {
-                    // where is the client?
+                    DocumentResult result = client.execute(index);
+                    if(result.isSucceeded()){
+                        tweet.setId(result.getId());
+                    }
+                    else{
+                        Log.i("Error", "Elasticsearch was not able to add the id");
+                    }
                 }
+
                 catch (Exception e) {
                     Log.i("Error", "The application failed to build and send the tweets");
                 }
@@ -38,7 +53,7 @@ public class ElasticsearchTweetController {
     }
 
     // TODO we need a function which gets tweets from elastic search
-/*    public static class GetTweetsTask extends AsyncTask<String, Void, ArrayList<NormalTweet>> {
+    public static class GetTweetsTask extends AsyncTask<String, Void, ArrayList<NormalTweet>> {
         @Override
         protected ArrayList<NormalTweet> doInBackground(String... search_parameters) {
             verifySettings();
@@ -46,8 +61,22 @@ public class ElasticsearchTweetController {
             ArrayList<NormalTweet> tweets = new ArrayList<NormalTweet>();
 
                 // TODO Build the query
-
+            Search search = new Search.Builder(search_parameters[0])
+                            .addIndex("testing")
+                            .addType("tweet")
+                            .build();
             try {
+                SearchResult result = client.execute(search);
+                if(result.isSucceeded()) {
+//                    List<NormalTweet> foundTweets=
+//                            result.getSourceAsObject(NormalTweet.class);
+                    List<NormalTweet> foundTweets = result.getSourceAsObjectList(NormalTweet.class);
+                    tweets.addAll(foundTweets);
+                }
+                else
+                {
+                    Log.i("Error","The search query failed");
+                }
                // TODO get the results of the query
             }
             catch (Exception e) {
@@ -56,7 +85,7 @@ public class ElasticsearchTweetController {
 
             return tweets;
         }
-    }*/
+    }
 
 
 
